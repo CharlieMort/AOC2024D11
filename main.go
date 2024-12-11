180b73a3e7094fb2d87efe32e451b516fbfa978d
charlie.mortimer@virginmediao2.co.uk <charlie.mortimer@virginmediao2.co.uk>
2024-12-11 13:57:00 +0000

    Working memo

--- main.go
+++ main.go
@@ -8,27 +8,31 @@ import (
 	"strings"
 )
 
-func Blink(num int, blinkNum int) []int {
-	if blinkNum > 75 {
-		return []int{num}
+var cache map[string]int
+
+func Blink(num int, blinkNum int) int {
+	val, ok := cache[fmt.Sprintf("%d/%d", num, blinkNum)]
+	if ok {
+		return val
+	}
+	if blinkNum == 0 {
+		return 1
 	}
 	numStr := fmt.Sprintf("%d", num)
 	if num == 0 {
-		return Blink(1, blinkNum+1)
+		return Blink(1, blinkNum-1)
 	} else if len(numStr)%2 == 0 {
 		leftHalf, _ := strconv.ParseInt(strings.Join(strings.Split(numStr, "")[:len(numStr)/2], ""), 10, 64)
 		rightHalf, _ := strconv.ParseInt(strings.Join(strings.Split(numStr, "")[len(numStr)/2:], ""), 10, 64)
-		out := make([]int, 0)
-		out = append(out, Blink(int(leftHalf), blinkNum+1)...)
-		out = append(out, Blink(int(rightHalf), blinkNum+1)...)
-		return out
+		cache[fmt.Sprintf("%d/%d", num, blinkNum)] = Blink(int(leftHalf), blinkNum-1) + Blink(int(rightHalf), blinkNum-1)
+		return Blink(int(leftHalf), blinkNum-1) + Blink(int(rightHalf), blinkNum-1)
 	} else {
-		return Blink(num*2024, blinkNum+1)
+		return Blink(num*2024, blinkNum-1)
 	}
 }
 
 func main() {
-	fmt.Println("AOC Day 11")
+	fmt.Println("AOC Day ")
 	dat, err := os.ReadFile("inputs.txt")
 	if err != nil {
 		log.Fatal(err)
@@ -40,10 +44,10 @@ func main() {
 		nums = append(nums, int(num))
 	}
 
-	out := make([]int, 0)
+	cache = make(map[string]int)
+	count := 0
 	for _, ele := range nums {
-		out = append(out, Blink(ele, 1)...)
+		count += Blink(ele, 75)
 	}
-
-	fmt.Println("Count:", len(out))
+	fmt.Println("Count:", count)
 }
